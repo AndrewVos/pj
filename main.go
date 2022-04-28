31471b8c1b430797043e504774756b86341d86cf
Andrew Vos <andrew.vos@gmail.com>
2022-04-28 13:12:58 +0100

    wip

--- main.go
+++ main.go
@@ -8,15 +8,19 @@ import "path"
 import "os"
 import "gopkg.in/yaml.v2"
 
-func readFile(path string) string {
+func readFile(path string) (string, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return string(data)
+	return string(data), nil
 }
 
 type Pacman struct {
+	Name []string
+}
+
+type Aur struct {
 	Name []string
 }
 
@@ -35,6 +39,7 @@ type Shell struct {
 
 type Fragment struct {
 	Pacman  Pacman
+	Aur     Aur
 	Symlink []Symlink
 	Script  []Script
 	Shell   Shell
@@ -44,22 +49,23 @@ func main() {
 	module_paths, err := filepath.Glob("../../.dotfiles/fragments/*")
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("error: %v", err)
 	}
 
 	for _, module_path := range module_paths {
 		configuration_path := path.Join(module_path, "configuration.yml")
 		fmt.Println(configuration_path)
 
-		if _, err := os.Stat(configuration_path); err == nil {
-			contents := readFile(configuration_path)
-
-			fragment := Fragment{}
-			err := yaml.Unmarshal([]byte(contents), &fragment)
-			if err != nil {
-				log.Fatalf("error: %v", err)
-			}
-			fmt.Printf("--- t:\n%+v\n\n", fragment)
+		contents, err := readFile(configuration_path)
+		if err != nil {
+			log.Fatalf("error: %v", err)
 		}
+
+		fragment := Fragment{}
+		err = yaml.Unmarshal([]byte(contents), &fragment)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		fmt.Printf("--- t:\n%+v\n\n", fragment)
 	}
 }
